f44b32166134e57212ca95dff9e841e02cf04ab0
David KRAME <kramedavid@gmail.com>
2023-05-26 07:29:47 +0200

    SIRN with distinct directories (2) FIX : Setting

--- Conformer/exp/exp_Model_SIRN.py
+++ Conformer/exp/exp_Model_SIRN.py
@@ -25,6 +25,19 @@ class Exp_Model:
     def __init__(self, args, setting):
         self.args = args
         self.setting = setting
+        self.device = self._acquire_device()
+        self.model = self._build_model().to(self.device)
+
+    def _acquire_device(self):
+        if self.args.use_gpu:
+            os.environ["CUDA_VISIBLE_DEVICES"] = str(
+                self.args.gpu) if not self.args.use_multi_gpu else self.args.devices
+            device = torch.device('cuda:{}'.format(self.args.gpu))
+            print('Use GPU: cuda:{}'.format(self.args.gpu))
+        else:
+            device = torch.device('cpu')
+            print('Use CPU')
+        return device
 
     def _build_model(self):
         model_dict = {
