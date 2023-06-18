f912f5157bcfb8e060fbd3451baa2ccabd04fbc5
David KRAME <kramedavid@gmail.com>
2023-06-18 21:56:08 +0200

    Add elec custom (correct bug 0-O)

--- scripts/Conformer/elec_custom/Conformer.sh
+++ scripts/Conformer/elec_custom/Conformer.sh
@@ -15,16 +15,16 @@ seq_len=96
 python -u ./Conformer/train.py \
     --data elec \
     --root_path ./dataset/ \
-    --data_path custom_elec_O1.csv \
+    --data_path custom_elec_01.csv \
     --patience $patience\
     --target "OT" \
-    --train_epochs $train_epochs >logs/Conformer/$model_name'_custom_elec_O1_'.log
+    --train_epochs $train_epochs >logs/Conformer/$model_name'_custom_elec_01_'.log
 
 python -u ./Conformer/train.py \
     --data elec \
     --root_path ./dataset/ \
-    --data_path custom_elec_O4.csv \
+    --data_path custom_elec_04.csv \
     --patience $patience\
     --target "OT" \
-    --train_epochs $train_epochs >logs/Conformer/$model_name'_custom_elec_O4_'.log
+    --train_epochs $train_epochs >logs/Conformer/$model_name'_custom_elec_04_'.log
 
