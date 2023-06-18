e8cc0e2dbcdb169405acb9132bfbf4849bb12735
David KRAME <kramedavid@gmail.com>
2023-06-18 09:12:12 +0200

    ETTh1 and ETTh2 first training

---
+++ scripts/Conformer/etth_1_2/Conformer.sh
@@ -0,0 +1,30 @@
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/Conformer" ]; then
+    mkdir ./logs/Conformer
+fi
+
+
+train_epochs=10
+patience=5
+model_name="Conformer"
+seq_len=96
+
+python -u ./Conformer/train.py \
+    --data ETTh1 \
+    --root_path ./dataset/ \
+    --data_path ETTh1.csv \
+    --patience $patience\
+    --target "OT" \
+    --train_epochs $train_epochs >logs/Conformer/$model_name'_etth1_'.log
+
+python -u ./Conformer/train.py \
+    --data ETTh2 \
+    --root_path ./dataset/ \
+    --data_path ETTh2.csv \
+    --patience $patience\
+    --target "OT" \
+    --train_epochs $train_epochs >logs/Conformer/$model_name'_etth2_'.log
+
