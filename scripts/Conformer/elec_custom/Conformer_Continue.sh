9b2a13ea97caa78ee1a14fb1727b2d93a2033eba
David KRAME <kramedavid@gmail.com>
2023-06-19 10:36:31 +0200

    Add elec custom continue

---
+++ scripts/Conformer/elec_custom/Conformer_Continue.sh
@@ -0,0 +1,20 @@
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/Conformer" ]; then
+    mkdir ./logs/Conformer
+fi
+
+train_epochs=3
+patience=2
+model_name="Conformer"
+seq_len=96
+
+python -u ./Conformer/train_Elec_Custom_Continue.py \
+    --data ECL \
+    --root_path ./dataset/ \
+    --data_path custom_elec_04.csv \
+    --patience $patience\
+    --target "OT" \
+    --train_epochs $train_epochs >logs/Conformer/$model_name'_Elec_Custom_Continue_01_on_04'.log
