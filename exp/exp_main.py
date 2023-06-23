0cf04acaf051ff74841337ab69e01b60e03658ec
David KRAME <kramedavid@gmail.com>
2023-06-23 11:10:04 +0200

    Elec_ continue (bug 2)

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -446,10 +446,24 @@ class Exp_Main(Exp_Basic):
         return
 
 
-class Exp_Main_Elec_Continue(Exp_Basic):
+class Exp_Main_Elec_Continue:
     def __init__(self, args, setting):
-        super(Exp_Main_Elec_Continue, self).__init__(args, setting)
-        # self.setting = setting
+        self.setting = setting
+        self.args = args
+        self.device = self._acquire_device()
+        self.model = self._build_model().to(self.device)
+
+    def _acquire_device(self):
+        if self.args.use_gpu:
+            os.environ["CUDA_VISIBLE_DEVICES"] = (
+                str(self.args.gpu) if not self.args.use_multi_gpu else self.args.devices
+            )
+            device = torch.device("cuda:{}".format(self.args.gpu))
+            print("Use GPU: cuda:{}".format(self.args.gpu))
+        else:
+            device = torch.device("cpu")
+            print("Use CPU")
+        return device
 
     def _build_model(self):
         model_dict = {
