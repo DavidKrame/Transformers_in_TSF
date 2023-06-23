3b4454624838a9db8815e32e66884dc3cef6e1e2
David KRAME <kramedavid@gmail.com>
2023-06-23 11:54:52 +0200

    ETTh_continue

---
+++ scripts/V_Transformer/etth_1_2/V_T_etth_Continue.sh
@@ -0,0 +1,37 @@
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/V_Transformer" ]; then
+    mkdir ./logs/V_Transformer
+fi
+
+model_name=Transformer
+seq_len=96
+
+for pred_len in 96 192 384 768
+do
+
+    python -u train_continue_Elec_Custom.py \
+      --is_training 1 \
+      --root_path ./dataset/ \
+      --data_path ETTh2.csv \
+      --model_id ETTh2_96_$pred_len \
+      --model $model_name \
+      --data ETTh2 \
+      --features MS \
+      --patience 2\
+      --seq_len 96 \
+      --label_len 48 \
+      --pred_len $pred_len \
+      --e_layers 2 \
+      --d_layers 1 \
+      --factor 3 \
+      --enc_in 321 \
+      --dec_in 321 \
+      --c_out 321 \
+      --des 'Exp' \
+      --itr 1 \
+      --train_epochs 3 >logs/V_Transformer/$model_name'_Continue_ETTh1_on_2'_$seq_len'_'$pred_len.log
+
+done
